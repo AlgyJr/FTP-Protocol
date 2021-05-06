f18712ae2d368fba6fc9935ea854c91648003144
Algy Jr <Algymussa@gmail.com>
2021-05-06 21:21:59 +0200

    feat(authentication): create client authentication package
    Authenticate client looking credentials.txt for a match credential to success authentication/login, and can get username authenticated

---
+++ Server/Auth/Authentication.java
@@ -0,0 +1,50 @@
+// package Server.Auth;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Authentication {
+    private String username;
+    private static String CREDENTIALS_FILE = "credentials.txt";
+
+    public Authentication() { 
+        this.username = "";
+     }
+
+    public boolean authenticate(String username, String password) {
+        StringTokenizer s;
+        String line, user, pass;
+        boolean hasMatched = false;
+
+        try {
+            FileReader fr = new FileReader(CREDENTIALS_FILE);
+            Scanner sc = new Scanner(fr);
+
+            while(sc.hasNextLine()) {
+                line = sc.nextLine();
+                s = new StringTokenizer(line, ":");
+                user = s.nextToken();
+                pass = s.nextToken();
+
+                // if matched break while and return true (success)
+                if (username.equals(user) && password.equals(pass)) {
+                    hasMatched = true;
+                    this.username = username;
+                    break;
+                }
+            }
+            sc.close();
+        } catch (FileNotFoundException fl) {
+            System.err.println("File not found!");
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+        return hasMatched;
+    }
+
+    public String getUsername() { return this.username; }
+}
