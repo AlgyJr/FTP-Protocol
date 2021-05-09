7f2db35962eb20b3bdb16264591e0d37b91eaf0d
nozotrox <fifasmazoio@gmail.com>
2021-05-10 00:52:26 +0200

    UPLOAD FINISHED

---
+++ Shared/Commands.java
@@ -0,0 +1,15 @@
+package Shared;
+
+public class Commands {
+    public static final String LS = "LS"; //::>> List Directories and Files
+    public static final String CD = "CD"; //::>> Move to new Folder or Path
+    public static final String PWD = "PWD"; //::>> Print Current Working Directory
+    public static final String STAT = "STAT";
+    public static final String MKDIR = "MKDIR"; //::>> Make a Directory
+    public static final String EXIT = "EXIT";
+    public static final String SETCHUNK = "SETCHUNK";
+    public static final String MVS = "MVS"; //::>> Move to server side
+    public static final String MVC = "MVC"; //::>> MOve to client side
+    public static final String GET = "GET"; //::>> Download File From Server
+    public static final String PUT = "PUT";
+}
